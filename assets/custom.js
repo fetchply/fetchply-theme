df8e8ad17e7393452567f94c9f4767db4123a1e4
ranaharoon3222 <ranaharoon3222@gmail.com>
2026-08-13 04:49:18 +0000

    Rebrand theme to Fetchply and remove license activation system
    - Delete the purchase-code/license check that phoned home to the vendor
      API and blocked the theme editor behind an activation overlay
    - Remove the Theme activate settings panel, purchase_code setting, and
      the vendor promo/affiliate panels and locale strings
    - Rename Ecomus/The4 to Fetchply across theme info, defaults and demo
      content, and drop vendor demo-store and affiliate tracking links
    
    Co-authored-by: Codesmith <codesmith-bot@users.noreply.github.com>

--- assets/custom.js
+++ assets/custom.js
@@ -11,7 +11,7 @@ var $wishlist_list = $id4('hdt_wishlist_list'),
   $compare_list = $id4('hdt_compare_list'),
   nameCachedWishlist = 'theme4:wishlist:id',
   nameCachedCompare = 'theme4:compare:id';
-// if exit $wishlist_list is use app wishlist the4
+// if exit $wishlist_list is use app wishlist fetchply
 if ($wishlist_list) {
   var arr_wishlist_list = $wishlist_list.textContent ? $wishlist_list.textContent.split(' ') : [];
 }
@@ -165,7 +165,7 @@ update_wis_btns_fn = function (_action) {
   // Update Count and link
   document.dispatchEvent(new CustomEvent(`theme4:${this.textFn}:update`, {
     bubbles: true,
-    detail: 'the4'  // arr_wishlist_list
+    detail: 'fetchply'  // arr_wishlist_list
   }));
   if ((window.isPageWishlist)) themeHDN.wisHref = conver_to_link_fn('wishlist', arr_wishlist_list);
 };
@@ -372,7 +372,7 @@ clear_all_fn = function () {
   // Update Count and link
   document.dispatchEvent(new CustomEvent(`theme4:${this.ofFn}:update`, {
     bubbles: true,
-    detail: 'the4'
+    detail: 'fetchply'
   }));
   this.array = [];
   $id4(`drawerCompare`)?.closest('hdt-drawer')?.close();
@@ -1297,7 +1297,7 @@ customElements.define('cart-countdown', cartCountdown);
 const contactForm = $$4('form[action^="/contact"]');
 contactForm.forEach(function(form) {
   form.addEventListener("submit", function(event){
-    sessionStorage.setItem("the4:recentform", this.getAttribute("id"));
+    sessionStorage.setItem("fetchply:recentform", this.getAttribute("id"));
   })
 });
 const goToForm = (id) => {
@@ -1309,10 +1309,10 @@ const goToForm = (id) => {
     buttonForm.scrollIntoView({ behavior: 'smooth' });
   }
 }
-var recentform = sessionStorage.getItem("the4:recentform") || "";
+var recentform = sessionStorage.getItem("fetchply:recentform") || "";
 
 if (location.href.indexOf('contact_posted=true') > 0 && recentform !== "" ) {
-  document.dispatchEvent( new CustomEvent('the4:recentform', { detail: { recentform: recentform }, bubbles: true, cancelable: true }) );
+  document.dispatchEvent( new CustomEvent('fetchply:recentform', { detail: { recentform: recentform }, bubbles: true, cancelable: true }) );
   $id4(recentform)?.classList.add("on-live");
   if ($4('.form-status-mirror-'+ recentform)) {
     $4('.form-status-mirror-'+ recentform).style.cssText = "display: block;";
@@ -1321,18 +1321,18 @@ if (location.href.indexOf('contact_posted=true') > 0 && recentform !== "" ) {
 }
 
 else if (location.href.indexOf('customer_posted=true') > 0 && recentform !== "" ) {
-  document.addEventListener('the4:recentform', (event)=>{
+  document.addEventListener('fetchply:recentform', (event)=>{
     let modal_id = document.getElementById(event.detail.recentform).getAttribute('data-id') || document.getElementById(event.detail.recentform).getAttribute('id');
     document.getElementById(modal_id)?.closest("hdt-modal:not([closed])")?.open();
   })
-  document.dispatchEvent( new CustomEvent('the4:recentform', { detail: { recentform: recentform }, bubbles: true, cancelable: true }) );
+  document.dispatchEvent( new CustomEvent('fetchply:recentform', { detail: { recentform: recentform }, bubbles: true, cancelable: true }) );
   $id4(recentform)?.classList.add("on-live");
   goToForm(recentform);
 }
 else if (location.href.indexOf('contact_posted=true') > 0 && location.href.indexOf('#') > 0 ) {
   var recentform = location.href.split("#")[1];
   if ($id4(recentform).length > 0) {
-    sessionStorage.setItem("the4:recentform", recentform);
+    sessionStorage.setItem("fetchply:recentform", recentform);
     $id4(recentform).classList.add("on-live");
     if ($4('.form-status-mirror-'+ recentform)) {
       $4('.form-status-mirror-'+ recentform).style.cssText = "display: block;";
@@ -1426,12 +1426,12 @@ class multiBrands extends HTMLElement {
           } else {
             $4(`#hdt-nav-ul-mb summary[data-url="${this.url}"]`)?.parentElement.setAttribute("open","");
           }
-          // localStorage.setItem("the4:brand:url", this.url);
+          // localStorage.setItem("fetchply:brand:url", this.url);
           break;
         }
       }
       // if(this.url === "") {
-      //   this.url = localStorage.getItem("the4:brand:url") || Shopify.routes.root;
+      //   this.url = localStorage.getItem("fetchply:brand:url") || Shopify.routes.root;
       //   for(var i=0; i < this.item.length; i++) {
       //     if (this.item[i].getAttribute("href") === this.url) {
       //       this.item[i].parentElement.classList.add("is--active");
@@ -1446,7 +1446,7 @@ class multiBrands extends HTMLElement {
     }
   }
   // _init(){
-  //   var nameStorage = "the4:brand:nav" + this.url.replace(/\//g,'-');
+  //   var nameStorage = "fetchply:brand:nav" + this.url.replace(/\//g,'-');
   //   if(!Shopify.designMode && sessionStorage.getItem(nameStorage) !== null){
   //     $id4("hdt-nav-ul").innerHTML = sessionStorage.getItem(nameStorage);
   //     $id4("hdt-nav-ul").classList.remove("hdt-nav-loading");
@@ -2185,145 +2185,6 @@ class countryFilter extends HTMLElement {
 customElements.define('country-filter', countryFilter);
 
 
-// check purchase code
-if(Shopify.designMode){
-
-  /**
-  * If it's a global variable then window[variableName] or in your case window["onlyVideo"] should do the trick.
-  * https://stackoverflow.com/questions/5613834/convert-string-to-variable-name-in-javascript
-  */
-  function isStorageSupported(type) {
-    // Return false if we are in an iframe without access to sessionStorage
-    // window.self !== window.top
-
-    var storage = (type === 'session') ? window.sessionStorage : window.localStorage;
-
-    try {
-      storage.setItem('t4s', 'test');
-      storage.removeItem('t4s');
-      return true;
-    } catch (error) {
-      return false;
-    }
-  };
-
-  var ThemeCode    = atob(window[atob('Y0hWeVkyaGg=')]),
-      ThemeName_base64 = window[atob('VkdobGJXVk9ZVzFsVkRR')],
-      ThemeName            = atob(ThemeName_base64),
-      CookieName           = 'SXNBY3RpdmVUaGVtZQ=='+ThemeName_base64,
-      ShopEmail            = atob(window[atob('VTJodmNFMWxiMVEw')]),
-      isTrueSet            = (sessionStorage.getItem(CookieName) === 'true' ),
-      str_temp_active      = atob('I3Q0cy10ZW1wLWtleS1hY3RpdmU='), // #t4s-temp-key-active
-      str_purchase         = atob('cHVyY2hhc2VfY29kZXQ0'); // purchase_codet4;
-
-      // console.log(ThemeCode,ThemeName,ShopEmail,CookieName,str_temp_active,str_purchase)
-  function alert_active_html() {
-    return `<section id="${str_purchase}" style="display: flex !important">${ $4(str_temp_active).innerHTML }</section>`;
-  };
-
-  // console.log('ThemeCode', ThemeCode, isTrueSet)
-
-  if (ThemeCode == '') {
-    let dom1 = (new DOMParser).parseFromString(alert_active_html(), "text/html");
-    document.body.append(dom1.body.firstElementChild);
-    let dom2 = (new DOMParser).parseFromString('<div id="luffyabc194"><style>body>*:not(#purchase_codet4) {opacity: 0;pointer-events: none;</style></div>', "text/html");
-    document.body.prepend(dom2.body.firstElementChild);
-    sessionStorage.removeItem(CookieName);
-    localStorage.removeItem(CookieName);
-  }
-  else if ( !isTrueSet ) {
-
-    //console.log(ShopEmail, ThemeName, ThemeCode);
-
-    var domain     = window.location.hostname,
-    mix        = ['4','t','h','e','p','l','i','c','o','/','.',':','n','s'],
-    mix_domain = mix[2]+mix[1]+mix[1]+mix[4]+mix[13]+mix[11]+mix[9]+mix[9]+mix[5]+mix[6]+mix[7]+mix[10]+mix[1]+mix[2]+mix[3]+mix[0]+mix[10]+mix[7]+mix[8]+mix[9]+mix[5]+mix[6]+mix[7]+mix[3]+mix[12]+mix[13]+mix[3]+mix[9]+mix[7]+mix[2]+mix[3]+mix[7]+'k',
-    data       = {
-      "shopify_domain": domain,
-      "email"         : ShopEmail,
-      "theme"         : ThemeName,
-      "purchase_code" : ThemeCode
-    };
-
-    fetch(mix_domain, {
-      "headers": {
-        "accept": "*/*",
-        "cache-control": "no-cache",
-        "x-requested-with": "XMLHttpRequest"
-      },
-      "body": btoa (encodeURIComponent(JSON.stringify(data))) ,
-      "method": "POST",
-      "mode": "cors"
-    })
-    .then(function(response) {
-      if(response.ok){
-        return response.json()
-      } throw ""
-    })
-    .then(function(response) {
-      let dom = (new DOMParser).parseFromString(alert_active_html(), "text/html");
-
-      if ( response.status == 1) {
-
-        dom.body.firstElementChild.innerHTML = "<p>ACTIVATED SUCCESSFULLY. Thanks for buying my theme!</p>";
-        document.body.append(dom.body.firstElementChild);
-
-        // Set a cookie to expire in 1 hour in Javascript
-        var isActived = localStorage.getItem(CookieName);
-        sessionStorage.setItem(CookieName, 'true')
-
-        if (isActived === 'true') {
-          $4(atob('I3B1cmNoYXNlX2NvZGV0NA==')).remove(); // #purchase_codet4
-          // $4(atob('I2x1ZmZ5YWJjMTk0'))?.remove(); //#luffyabc194
-        }
-        else {
-          localStorage.setItem(CookieName, "true");
-          setTimeout(function(){
-            $4(atob('I3B1cmNoYXNlX2NvZGV0NA==')).remove(); // #purchase_codet4
-            // $4(atob('I2x1ZmZ5YWJjMTk0'))?.remove(); //#luffyabc194
-          }, 1000);
-        }
-
-      }
-      else {
-
-        var mess = response.message;
-        if (mess.includes("No sale belonging to the current user found with that code")) {
-
-          dom.body.firstElementChild.innerHTML = "<p>Purchase code error. It is a sales reversal or a refund. :(((</p>";
-
-        }
-        else if (mess.includes("That license key doesn't appear to be valid") || mess.length == 58 || mess.length == 101) {
-          dom.body.firstElementChild.innerHTML = "<p>That license key doesn't appear to be valid. Please check your purchase code again!<br>Please open a ticket at <a href='https://support.the4.co' target='_blank'><span>support.the4.co</span></a> if you have any question.</p>";
-
-        }
-        else if (mess.includes("The license code not match with current theme") || mess.length == 104) {
-          dom.body.firstElementChild.innerHTML = "<p>The license not match with current theme.!<br>Please open a ticket at <a href='https://support.the4.co' target='_blank'><span>support.the4.co</span></a> if you have any question.</p>";
-        }
-        else if (mess.includes("That license key has been invalidated, due to being active domain")) {
-          try {
-            var mess = mess.split('active domain `')[1].split('`. ')[0];
-          }
-          catch(err) {
-            //var mess = mess;
-          }
-          dom.body.firstElementChild.innerHTML = "<p>Your purchase code is invalided since it is being activated at another store "+mess+".<br> Please open a ticket at <a class='cg' href='https://support.the4.co' target='_blank'><span>support.the4.co</span></a> to get quick assistance.</p>";
-        }
-        else {
-          dom.body.firstElementChild.innerHTML = "<p>" + mess + ".<br> Please open a ticket at <a href='https://support.the4.co' target='_blank'><span>support.the4.co</span></a> if you have any question.</p>";
-        }
-        document.body.append(dom.body.firstElementChild);
-
-      }
-
-    }).catch(function(e) {
-    //}).catch((e)=>{
-      console.error(e)
-    });
-
-  }
-}
-// end check purchase code
 
 // readmore collection description
 class DescriptionShort extends HTMLElement{
